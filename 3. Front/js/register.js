d18489a1ebfa880ee9b130f7d1e7c0332ddc998c
zlzzlzz2l <zlzzlzz2l@naver.com>
2020-10-15 00:33:30 +0900

    convenient.html

--- 3. Front/js/register.js
+++ 3. Front/js/register.js
@@ -1,4 +1,4 @@
-$(".login-form").validate({
+/**$(".login-form").validate({
     rules: {
       username: {
         required: true,
@@ -34,4 +34,44 @@ $(".login-form").validate({
         error.insertAfter(element);
       }
     }
-  });
+  });**/
+
+  $(document).ready(function(){
+    $("#SignUP").click(function(){
+        var json = {
+            m_id : $("#username").val(),
+            m_pw : $("password").val()
+        };
+        
+        for(var str in json){
+            if(json[str].length == 0){
+                alert($("#" + str).attr("placeholder") + "를 입력해주세요.");
+                $("#" + str).focus();
+                return;
+            }
+        }
+        
+         $.ajax({
+            type : "post",
+            url : "${pageContext.request.contextPath}/idCheck.do",
+            data : json,
+            success : function(data) {
+                switch (Number(data)) {
+                case 0:
+                    alert("정상적으로 회원가입 되었습니다.");
+                    window.location.href = "/demo";
+                    break;
+                case 1:
+                    alert("아이디가 중복 되었습니다.");
+                    break;
+                default:
+                    alert("알수없는 오류가 발생 했습니다. [Error Code :" + Number(data) + "]");
+                    break;
+                }
+            },
+            error : function(error) {
+                alert("오류 발생"+ error);
+            }
+        });
+    });
+});
