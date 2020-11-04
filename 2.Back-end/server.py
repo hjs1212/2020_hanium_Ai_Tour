61f8ccef184cd8ed436e852123b333869cee7f50
hjs1212 <38662922+hjs1212@users.noreply.github.com>
2020-11-04 19:28:04 +0900

    Update server.py

--- 2.Back-end/server.py
+++ 2.Back-end/server.py
@@ -14,8 +14,7 @@ server_socket.bind((HOST, PORT))
 image_w = 127
 image_h = 127
 
-korModel = keras.models.load_model("kor.h5")
-engModel = keras.models.load_model("eng.h5")
+Model = keras.models.load_model("kor.h5")
 
 reSizing = lambda img : cv2.resize(img, None, fx=image_w/img.shape[1], fy=image_h/img.shape[0])
 
@@ -41,25 +40,20 @@ def connUser(client_socket, addr) :
             X.append(reSizing(img)/256)
             X = np.array(X)
             msg = ''
-            if useLang == '10' :
-                pred = korUser(X)
-                if pred == [] :
-                    msg = 0
-                else :
+            pred = User(X)
+            if len(pred) == 0 :
+                msg = 0
+            else :
+                if useLang == '10' :
                     msg = korCategories[pred[0]]
-            elif useLang == '20' :
-                pred = engUser(img)
-                if pred == [] :
-                    msg = 0
-                else :
+                elif useLang == '20' :
                     msg = engCategories[pred[0]]
             client_socket.sendall(str(msg).encode())
         except Exception as e:
             break
     client_socket.close()
 
-korUser = lambda img : korModel.predict_classes(img)
-engUser = lambda img : engModel.predict_classes(img)
+User = lambda img : Model.predict_classes(img)
 korCategories = [126508, 126535, 128019, 126474, 129438, 250331, 264590, 126451, 126175, 127923]
 engCategories = [264377, 264550, 264244, 264222, 616253, 264421, 264591, 1052165, 264238, 264128]
 
